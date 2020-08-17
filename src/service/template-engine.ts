735627174a728b4cdfa47b3b391a61883a48f5c1
Danial Manavi <dmanavi@live.com>
2020-08-17 17:09:22 +0430

    Add comment to the template engine.
    Changes to be committed:
    	modified:   src/service/template-engine.ts

--- src/service/template-engine.ts
+++ src/service/template-engine.ts
@@ -4,9 +4,14 @@
 
 import { render as renderTemplate } from 'ejs';
 
-export const render = async <T>(template: string, metadata?: T): Promise<string> =>
+/**
+ * Render a template and return the rendered content
+ * @param template Template to render (string that contains the template)
+ * @param data Data to pass to the render engine
+ */
+export const render = async <T>(template: string, data?: T): Promise<string> =>
     await renderTemplate(
         template,
-        metadata,
+        data,
         { async: true, }
     );
