8a360936f8fd79e44d9d0c4677cbec8ab4215d22
Danial Manavi <dmanavi@live.com>
2020-08-17 17:29:40 +0430

    Improve migrations table.
    Changes to be committed:
    	modified:   resources/sql/0001_migrations-table.sql

--- resources/sql/0001_migrations-table.sql
+++ resources/sql/0001_migrations-table.sql
@@ -7,16 +7,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
 
 CREATE TABLE IF NOT EXISTS <%- migration.tableName %>
 (
-	id uuid default uuid_generate_v4() not null
+	record_id uuid default uuid_generate_v4() not null
 		constraint <%- migration.tableName %>_pk
 			primary key,
-	filename char(127) not null,
+	identifier char(127) not null,
 	timestamp timestamp default now()
 );
 
 COMMENT ON TABLE <%- migration.tableName %> IS 'Applied migrations';
 
-COMMENT ON COLUMN <%- migration.tableName %>.filename IS 'File name of the migration script';
+COMMENT ON COLUMN <%- migration.tableName %>.identifier IS 'A unique identifier for the migration';
 COMMENT ON COLUMN <%- migration.tableName %>.timestamp IS 'Time that migration has taken place';
 
-CREATE UNIQUE INDEX IF NOT EXISTS <%- migration.tableName %>_filename_uindex on <%- migration.tableName %> (filename);
+CREATE UNIQUE INDEX IF NOT EXISTS <%- migration.tableName %>_identifier_uindex on <%- migration.tableName %> (identifier);
