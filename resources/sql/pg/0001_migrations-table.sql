5c6fccd2e3b956b4d31940edc154d5bfee28914c
Danial Manavi <dmanavi@live.com>
2020-08-25 21:43:29 +0430

    Improve SQL script.
    Changes to be committed:
    	modified:   resources/sql/pg/0001_migrations-table.sql

--- resources/sql/pg/0001_migrations-table.sql
+++ resources/sql/pg/0001_migrations-table.sql
@@ -1,22 +1,25 @@
 
+-- Schema
+CREATE SCHEMA IF NOT EXISTS <%- migration.schemaName %>;
+
 -- extensions
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA <%- migration.schemaName %>
 
 -- migrations table
 
-CREATE TABLE IF NOT EXISTS <%- migration.tableName %>
+CREATE TABLE IF NOT EXISTS <%- migration.schemaName %>.<%- migration.tableName %>
 (
-	record_id uuid default uuid_generate_v4() not null
+	record_id uuid default <%- migration.schemaName %>.uuid_generate_v4() not null
 		constraint <%- migration.tableName %>_pk
 			primary key,
 	identifier char(127) not null,
 	timestamp timestamp default now()
 );
 
-COMMENT ON TABLE <%- migration.tableName %> IS 'Applied migrations';
+COMMENT ON TABLE <%- migration.schemaName %>.<%- migration.tableName %> IS 'Applied migrations';
 
-COMMENT ON COLUMN <%- migration.tableName %>.identifier IS 'A unique identifier for the migration';
-COMMENT ON COLUMN <%- migration.tableName %>.timestamp IS 'Time that migration has taken place';
+COMMENT ON COLUMN <%- migration.schemaName %>.<%- migration.tableName %>.identifier IS 'A unique identifier for the migration';
+COMMENT ON COLUMN <%- migration.schemaName %>.<%- migration.tableName %>.timestamp IS 'Time that migration has taken place';
 
-CREATE UNIQUE INDEX IF NOT EXISTS <%- migration.tableName %>_identifier_uindex on <%- migration.tableName %> (identifier);
+CREATE UNIQUE INDEX IF NOT EXISTS <%- migration.tableName %>_identifier_uindex on <%- migration.schemaName %>.<%- migration.tableName %> (identifier);
