27db66d08ede33c16661d8d6ec25d1a59b16fb3c
Danial Manavi <dmanavi@live.com>
2020-08-26 13:10:49 +0430

    Prepare scripts.
    Changes to be committed:
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   test/provider/migration/file-system.ts

---
+++ test/provider/migration/file-system.ts
@@ -0,0 +1,8 @@
+
+import 'mocha';
+
+describe('[KeenDev] [DB Migration CLI] [Provider] [Repository] [Migration]', () => {
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	it('should be OK', () => { });
+});
