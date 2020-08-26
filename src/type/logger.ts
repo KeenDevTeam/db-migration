5665851d5456f0007c6038e6ee63cedf8c57f2fe
Danial Manavi <dmanavi@live.com>
2020-08-26 12:56:36 +0430

    Convert type to interface.
    Changes to be committed:
    	modified:   src/type/logger.ts

--- src/type/logger.ts
+++ src/type/logger.ts
@@ -5,7 +5,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type LogFunction = (msg: string, ...args: Array<any>) => void;
 
-export type Logger = {
+export interface Logger {
 
 	info: LogFunction,
 	debug: LogFunction,
