e0583e813b6e4ededf9d2c46e4aa021a570b5248
Danial Manavi <dmanavi@live.com>
2020-08-26 12:42:03 +0430

    Logger type.
    Changes to be committed:
    	new file:   src/type/logger.ts

---
+++ src/type/logger.ts
@@ -0,0 +1,17 @@
+/**
+ * Logger
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type LogFunction = (msg: string, ...args: Array<any>) => void;
+
+export type Logger = {
+
+	info: LogFunction,
+	debug: LogFunction,
+	warning: LogFunction,
+	success: LogFunction,
+	error: LogFunction,
+
+	loading: LogFunction,
+}
