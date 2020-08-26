1d7e44c698ffd65dbaa3c182f266968f3bb71f39
Danial Manavi <dmanavi@live.com>
2020-08-26 13:13:51 +0430

    Add lint ignore.
    Changes to be committed:
    	modified:   src/provider/log/console.ts

--- src/provider/log/console.ts
+++ src/provider/log/console.ts
@@ -11,29 +11,29 @@ export class ConsoleLogger implements Logger {
 	private readonly logger: ora.Ora = ora();
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	info(msg: string, ...args: Array<any>): void {
+	info(msg: string, ...args: Array<any>): void { // eslint-disable-line @typescript-eslint/no-unused-vars
 		this.logger.info(msg);
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	debug(msg: string, ...args: Array<any>): void {
+	debug(msg: string, ...args: Array<any>): void { // eslint-disable-line @typescript-eslint/no-unused-vars
 		throw new Error('NotImplemented');
 	}
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	warning(msg: string, ...args: Array<any>): void {
+	warning(msg: string, ...args: Array<any>): void { // eslint-disable-line @typescript-eslint/no-unused-vars
 		this.logger.warn(msg);
 	}
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	success(msg: string, ...args: Array<any>): void {
+	success(msg: string, ...args: Array<any>): void { // eslint-disable-line @typescript-eslint/no-unused-vars
 		this.logger.succeed(msg);
 	}
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	error(msg: string, ...args: Array<any>): void {
+	error(msg: string, ...args: Array<any>): void { // eslint-disable-line @typescript-eslint/no-unused-vars
 		this.logger.fail(msg);
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	loading(msg: string, ...args: Array<any>): void {
+	loading(msg: string, ...args: Array<any>): void { // eslint-disable-line @typescript-eslint/no-unused-vars
 		this.logger.start(msg);
 	}
 }
