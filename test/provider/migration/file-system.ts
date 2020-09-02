f936a7ca7aa762be04b61cddf04d804f61633ab8
Danial Manavi <dmanavi@live.com>
2020-09-03 00:39:48 +0430

    Add sample test.
    Changes to be committed:
    	modified:   test/provider/migration/file-system.ts

--- test/provider/migration/file-system.ts
+++ test/provider/migration/file-system.ts
@@ -1,7 +1,7 @@
 
 import 'mocha';
 
-describe('[KeenDev] [DB Migration CLI] [Provider] [Repository] [Migration]', () => {
+describe('[KeenDev][DB Migration][Provider][Repository][Migration]', () => {
 
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	it('should be OK', () => { });
