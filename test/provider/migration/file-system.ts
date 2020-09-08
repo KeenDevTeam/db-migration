37c9191ad0e3ee7df7212b0a5da06348c28f050f
Danial Manavi <dmanavi@live.com>
2020-09-08 19:41:45 +0430

    Remove an old file (renamed).
    Changes to be committed:
    	deleted:    test/provider/migration/file-system.ts

--- test/provider/migration/file-system.ts
+++
@@ -1,8 +0,0 @@
-
-import 'mocha';
-
-describe('[KeenDev][DB Migration][Provider][Repository][Migration]', () => {
-
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	it('should be OK', () => { });
-});
