04ad345bd25af5380480d8e7ea757f922195261c
Danial Manavi <dmanavi@live.com>
2020-09-08 18:36:39 +0430

    Module structure test.
    Changes to be committed:
    	new file:   test/index.spec.ts

---
+++ test/index.spec.ts
@@ -0,0 +1,23 @@
+/**
+ * Module entry point tests
+ */
+
+import 'mocha';
+
+import { expect, } from 'chai';
+
+import * as mdl from '../src';
+
+describe('[KeenDev][DB Migration][Module]', () => {
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	it('Testing module structure', () => {
+
+		expect(Object.keys(mdl)).to.have.lengthOf(4);
+
+		expect(mdl).to.have.property('migration').that.is.an('object');
+		expect(mdl).to.have.property('repository').that.is.an('object');
+		expect(mdl).to.have.property('template').that.is.an('object');
+		expect(mdl).to.have.property('util').that.is.an('object');
+	});
+});
