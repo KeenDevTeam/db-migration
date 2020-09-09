e639d67fd69765efcc434a7d861c2930791455eb
Danial Manavi <dmanavi@live.com>
2020-09-09 20:18:27 +0430

    Postgres driver test.
    Changes to be committed:
    	new file:   test/migration/pg.spec.env
    	new file:   test/migration/pg.spec.ts
    	new file:   test/mock/migration-repo.ts

---
+++ test/mock/migration-repo.ts
@@ -0,0 +1,88 @@
+/**
+ * Mock migration repo
+ */
+
+import { MigrationRepository } from '../../src/type/migration-repository';
+import { MigrationScript } from '../../src/type/migration-script';
+
+export const create = (): MigrationRepository => ({
+    /**
+     * Load all available migration scripts from the repository
+     */
+	loadAll: async (): Promise<Array<MigrationScript>> => ([
+		{
+			id: 'mig-1',
+			script: 'SELECT 1',
+		},
+		{
+			id: 'mig-2',
+			script: 'SELECT 2',
+		},
+		{
+			id: 'mig-3',
+			script: 'SELECT 3',
+		}
+	]),
+
+    /**
+     * Create a blank migration
+     * @param humanFriendlyName A name for the migration (e.g. Add users table)
+     * @param content Migration script content
+     */
+	create: async (humanFriendlyName: string, content: string): Promise<void> => { }
+});
+
+export const createWithInvalidSQLSyntaxAtFirstIndex = (): MigrationRepository => ({
+    /**
+     * Load all available migration scripts from the repository
+     */
+	loadAll: async (): Promise<Array<MigrationScript>> => ([
+		{
+			id: 'mig-1',
+			script: 'SELECT * from invalid.obj',
+		},
+		{
+			id: 'mig-2',
+			script: 'SELECT 2',
+		},
+		{
+			id: 'mig-3',
+			script: 'SELECT 3',
+		},
+	]),
+
+    /**
+     * Create a blank migration
+     * @param humanFriendlyName A name for the migration (e.g. Add users table)
+     * @param content Migration script content
+     */
+	create: async (humanFriendlyName: string, content: string): Promise<void> => { }
+});
+
+export const createWithInvalidSQLSyntaxAtLastIndex = (): MigrationRepository => ({
+    /**
+     * Load all available migration scripts from the repository
+     */
+	loadAll: async (): Promise<Array<MigrationScript>> => ([
+		{
+			id: 'mig-1',
+			script: 'SELECT 1',
+		},
+		{
+			id: 'mig-2',
+			script: 'SELECT 2',
+		},
+		{
+			id: 'mig-3',
+			script: 'SELECT * from invalid.obj',
+		},
+	]),
+
+    /**
+     * Create a blank migration
+     * @param humanFriendlyName A name for the migration (e.g. Add users table)
+     * @param content Migration script content
+     */
+	create: async (humanFriendlyName: string, content: string): Promise<void> => { }
+});
+
